bd916fab8448a0bc7d7fe7f407853b20a931cde5
Simon <krenich.szymon@gmail.com>
2023-01-22 23:14:47 +0100

    Created pagination for users and projects app

--- projects/views.py
+++ projects/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Project, Tag
+from .models import Project
 from .forms import ProjectForm
-from django.db.models import Q
-from .utils import searchProjects
+from .utils import searchProjects, paginateProjects
 
 
 def projects(request):
     projects, search_query = searchProjects(request)
-    context={'projects':projects, 'search_query':search_query}
+    
+    custom_range, projects = paginateProjects(request, projects, 3)
+    
+    context={'projects':projects, 'search_query':search_query, 'custom_range':custom_range}
     return render(request, 'projects/projects.html', context)
 
 
