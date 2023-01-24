166b4eab65ad4fa87b5a90aee20823c001afccd9
Simon <krenich.szymon@gmail.com>
2023-01-24 21:47:36 +0100

    Migration for Review, fixed TypeError: Field.__init__()

--- projects/models.py
+++ projects/models.py
@@ -2,6 +2,7 @@ from email.policy import default
 from django.db import models
 import uuid
 from users.models import Profile
+from django.utils import timezone
 
 class Project(models.Model):
     owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
@@ -28,10 +29,15 @@ class Review(models.Model):
         ('up', 'Up Vote'),
         ('down', 'Down Vote'),
     )
-    #owner = 
+    owner = models.ForeignKey(Profile, models.CASCADE, null=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     body  = models.TextField(null=True, blank=True)
-    value = models.CharField(max_length=200) 
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    class Meta:
+        unique_together = [['owner', 'project']]
 
     def __str__(self):
         return self.value
